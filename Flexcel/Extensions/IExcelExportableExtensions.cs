a2ca9621cc38f13b7fa82aa6aaab3ea923de6351
KONTUR\i.bannikov <i.bannikov@skbkontur.ru>
2022-07-27 11:41:05 +0500

    Немного подправил экстеншены

--- Flexcel/Extensions/IExcelExportableExtensions.cs
+++ Flexcel/Extensions/IExcelExportableExtensions.cs
@@ -2,21 +2,23 @@
 
 namespace Flexcel.Extensions;
 
-public static class IExcelExportableExtensions
+public static class ExcelExportableExtensions
 {
+    public static byte[] ToByteArray(this IExcelExportable fluent) => fluent.GetPackage().GetAsByteArray();
+    
     public static void ToFile(this IExcelExportable fluent, string filePath)
     {
         using var fs = new FileStream(filePath, FileMode.Create, FileAccess.Write);
-        var bytes = fluent.GetPackage().GetAsByteArray();
+        var bytes = fluent.ToByteArray();
         fs.Write(bytes, 0, bytes.Length);
     }
     
     public static Task ToFileAsync(this IExcelExportable fluent, string filePath)
     {
         using var fs = new FileStream(filePath, FileMode.Create, FileAccess.Write);
-        var bytes = fluent.GetPackage().GetAsByteArray();
+        var bytes = fluent.ToByteArray();
         return fs.WriteAsync(bytes, 0, bytes.Length);
     }
 
-    public static byte[] ToByteArray(this IExcelExportable fluent) => fluent.GetPackage().GetAsByteArray();
+    public static MemoryStream ToMemoryStream(this IExcelExportable fluent) => new MemoryStream(fluent.ToByteArray());
 }
